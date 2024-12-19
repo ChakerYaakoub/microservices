e5fe785c152ca44adca41debb6143614f7c76a50
ChakerYaakoub <117543062+ChakerYaakoub@users.noreply.github.com>
2024-12-19 13:02:19 +0100

    first commit

---
+++ service-gateway/src/test/java/service_gateway/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package service_gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-medical-record/src/test/java/service_medical_record/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package service_medical_record.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
