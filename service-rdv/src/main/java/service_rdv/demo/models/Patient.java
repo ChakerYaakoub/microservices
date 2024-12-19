ec82bbc930eebea674a61d11b59106570342366e
ChakerYaakoub <117543062+ChakerYaakoub@users.noreply.github.com>
2024-12-19 17:43:26 +0100

    service-rdv : version 2

--- service-rdv/src/main/java/service_rdv/demo/models/Patient.java
+++ service-rdv/src/main/java/service_rdv/demo/models/Patient.java
@@ -23,5 +23,75 @@ public class Patient {
         this.id = id;
     }
 
-    // Autres getters et setters...
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getNumeroSecu() {
+        return numeroSecu;
+    }
+
+    public void setNumeroSecu(String numeroSecu) {
+        this.numeroSecu = numeroSecu;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
 }
--- service-rdv/src/main/java/service_rdv/demo/models/Praticien.java
+++ service-rdv/src/main/java/service_rdv/demo/models/Praticien.java
@@ -33,5 +33,27 @@ public class Praticien {
         this.nom = nom;
     }
 
-    // Autres getters et setters...
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
